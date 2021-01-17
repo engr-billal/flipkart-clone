bfe6f535ffc53bbd44a4b9af18d76b56ca964db1
Muhammad Bilal <hafizbali315@gmail.com>
2021-01-17 20:48:32 +0500

    deploy on github

---
+++ src/urlConfig.js
@@ -0,0 +1,12 @@
+// const baseUrl = "https://flipkart-rest-server.herokuapp.com";
+
+// export const api = `${baseUrl}/api`;
+// export const generatePublicUrl = (fileName) => {
+//   return `${baseUrl}/public/${fileName}`;
+// };
+
+export const api = 'http://localhost:2000/api';
+
+export const generatePublicUrl = (fileName) => {
+	return `http://localhost:2000/public/${fileName}`;
+};
